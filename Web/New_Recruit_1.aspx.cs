17fee199057afe12d84a5a8378c1109d4a9280d9
dong hao <13301147@bjtu.edu.cn>
2016-06-23 20:32:58 +0800

    新增挑战_1

---
+++ Web/New_Recruit_1.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web
+{
+    public partial class New_Recruit_1 : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void new_challenge_btn_1_Click(object sender, EventArgs e)
+        {
+            String type = RadioButtonList1.SelectedValue;
+            String select = "1";
+            String not_select = "0";
+            String Camera = not_select;
+            if (is_camera.Checked)
+                Camera = select;
+            String Public = not_select;
+            if (is_public.Checked)
+                Public = select;
+        }
+    }
+}
